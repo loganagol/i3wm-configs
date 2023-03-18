bce3ad212548826cab9d04907fa549e65d39fdd3
Logan <lafeid.314@gmail.com>
2023-03-18 10:17:26 -0400

    Git push from script.

--- runthistobackup.sh
+++ runthistobackup.sh
@@ -3,10 +3,10 @@
 # links but I had to update links every time I added or removed helper
 # scripts from the directories.
 
-cp $HOME/.config/i3/ 		$HOME/projects/dot-config-i3wm/i3/
-cp $HOME/.config/i3status/ 	$HOME/projects/dot-config-i3wm/i3status/
-cp $HOME/.config/rofi/ 		$HOME/projects/dot-config-i3wm/rofi/
-cp $HOME/.config/dunst/		$HOME/projects/dot-config-i3wm/dunst/
+cp -r $HOME/.config/i3/ 	$HOME/projects/dot-config-i3wm/i3/
+cp -r $HOME/.config/i3status/ 	$HOME/projects/dot-config-i3wm/i3status/
+cp -r $HOME/.config/rofi/ 	$HOME/projects/dot-config-i3wm/rofi/
+cp -r $HOME/.config/dunst/	$HOME/projects/dot-config-i3wm/dunst/
 git add .
 git commit -m 'Git push from script.'
 git push origin master
