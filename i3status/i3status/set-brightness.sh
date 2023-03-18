e599902418650eb9b7af8a5e969ae8d5c16eea98
Logan <lafeid.314@gmail.com>
2023-03-18 10:19:30 -0400

    Git push from script.

---
+++ i3status/i3status/set-brightness.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script is called by i3 keybinds to set brightness via brightnessctl.
+# Afterwards, it does math to define brightness as a percentage and writes
+# that number to current_brightness. i3status then reads this file and outputs
+# its value to the status bar.
+
+curr_percent=$(cat $HOME/.config/i3status/current_brightness)
+
+if [[ $1 == "up" ]]; then
+	if [[ $curr_percent -le 10 ]]; then
+		brightnessctl set +1%
+	else
+		brightnessctl set +10%
+	fi
+else
+	if [[ $curr_percent -le 10 ]]; then
+		brightnessctl set 1-%
+	else
+		brightnessctl set 10-%
+	fi
+fi
+
+current=$(cat /sys/class/backlight/intel_backlight/brightness)
+max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+
+percentage=$((current * 100 / max))
+echo $percentage > $HOME/.config/i3status/current_brightness
